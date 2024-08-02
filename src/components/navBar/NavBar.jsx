220754ade7ca08d85a338dfd0639dfb855e56ebd
ashish <ashishsaharia04@gmail.com>
2024-08-02 11:55:56 +0530

    added the doh setup button

--- src/components/navBar/NavBar.jsx
+++ src/components/navBar/NavBar.jsx
@@ -1,4 +1,5 @@
 import JoinNow from "../buttons/joinNowButton";
+import { Link } from "react-router-dom";
 import NavFooterButton from '../buttons/nav-footerButton'
 import Hamburger from '../hamburger/hamburger.jsx';
 import Logo from '../logo/logo.jsx';
@@ -16,6 +17,8 @@ export default function Navbar(){
                     <NavFooterButton namePlaceholder={"About"}></NavFooterButton>
                     <NavFooterButton namePlaceholder={"Contact"}></NavFooterButton>
                     <NavFooterButton namePlaceholder={"Sign Up"}></NavFooterButton>
+                    <Link to='/agaaminAfricanHomePage/dohSetup'><NavFooterButton namePlaceholder={"DOH Setup"}></NavFooterButton></Link>
+
                 </div>
                     <JoinNow></JoinNow>
                     <div className="hamburgerWrapper">
